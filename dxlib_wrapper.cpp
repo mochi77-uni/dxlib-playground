c3c11cc24c423db4dfd5dda34d8bc686a73d1136
mochi77-uni <mochi77.uni@gmail.com>
2025-07-28 17:31:04 +0800

    temp

--- dxlib_wrapper.cpp
+++ dxlib_wrapper.cpp
@@ -5,13 +5,32 @@
 #include "dxlib_wrapper.h"
 
 #include <stdexcept>
-#include <DxLib.h>
 
 DxLibWrapper::DxLibWrapper() {
-	if (DxLib_Init() == DxFailure)
+	if (DxLib::DxLib_Init() == DxFailure)
 		throw std::runtime_error("DxLib initialize failed");
 }
 
 DxLibWrapper::~DxLibWrapper() {
-	DxLib_End();
+	DxLib::DxLib_End();
+}
+
+DxWindowModeResult DxLibWrapper::ChangeWindowMode(DxWindowMode mode) {
+	return static_cast<DxWindowModeResult>(DxLib::ChangeWindowMode(static_cast<int>(mode)));
+}
+
+int DxLibWrapper::GetWindowSize(glm::vec2& outSize) {
+	int width, height;
+	const int ret = DxLib::GetWindowSize(&width, &height);
+	outSize.x = width;
+	outSize.y = height;
+	return ret;
+}
+
+int DxLibWrapper::GetMousePoint(glm::vec2& outLocation) {
+	int x, y;
+	const int ret = DxLib::GetMousePoint(&x, &y);
+	outLocation.x = x;
+	outLocation.y = y;
+	return ret;
 }
--- dxlib_wrapper.h
+++ dxlib_wrapper.h
@@ -5,6 +5,23 @@
 #ifndef DXLIB_WRAPPER_H
 #define DXLIB_WRAPPER_H
 
+#include <windows.h>
+#include <glm/glm.hpp>
+
+#define DX_NON_USING_NAMESPACE_DXLIB
+#include <DxLib.h>
+
+enum class DxWindowMode : int {
+	Windowed = TRUE,
+	FullScreen = FALSE
+};
+
+enum class DxWindowModeResult : int {
+	Ok = DX_CHANGESCREEN_OK,
+	Return = DX_CHANGESCREEN_RETURN,
+	Default = DX_CHANGESCREEN_DEFAULT
+};
+
 class DxLibWrapper {
 	// シングルトンパターンのためコンストラクターは公開しない
 	DxLibWrapper();
@@ -25,6 +42,10 @@ public:
 
 	static constexpr int DxSuccess = 0;
 	static constexpr int DxFailure = -1;
+
+	static DxWindowModeResult ChangeWindowMode(DxWindowMode mode);
+	static int GetWindowSize(glm::vec2& outSize);
+	static int GetMousePoint(glm::vec2& outLocation);
 };
 
 
